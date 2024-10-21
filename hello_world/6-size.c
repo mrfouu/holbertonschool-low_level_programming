decf4b9202afc85bb29237b346c2c46af83a6622
mrfouu <bryanweinegaessel@gmail.com>
2024-10-21 13:27:03 +0000

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float d;
 	float f;
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
