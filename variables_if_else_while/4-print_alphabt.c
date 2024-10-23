d5c2e09d435253f112fdd38e815a9ea6b990639b
mrfouu <bryanweinegaessel@gmail.com>
2024-10-23 11:46:12 +0200

    task4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 	{
-		if (alphabet != 'q' && alphabe t != 'e')
+		if (alphabet != 'q' && alphabet != 'e')
 			putchar(alphabet);
 	}
 	putchar('\n');
