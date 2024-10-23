7df1c9e686fd016e7a53a4632d45cd00bc3e48b0
mrfouu <bryanweinegaessel@gmail.com>
2024-10-23 14:53:27 +0200

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
-* main -random number
-*
-* Return: 0
-*/
-
+ * main-Numberz
+ *
+ * Return: 0
+ */
 int main(void)
 {
-        int a;
+	int a;
 
-        for (a = 48; a < 58; a++)
-                putchar(a);
+	for (a = 48; a < 58; a++)
+		putchar(a);
+	putchar('\n');
 
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
