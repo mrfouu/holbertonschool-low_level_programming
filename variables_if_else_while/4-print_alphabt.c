92a15bd46f54c50c1a06eae8912baa78036abb8a
mrfouu <bryanweinegaessel@gmail.com>
2024-10-23 11:44:15 +0200

    task4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -random number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		if (alphabet != 'q' && alphabe t != 'e')
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
