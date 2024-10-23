887daaf532c622ebf83e2c93c7faec8b2d7338a1
mrfouu <bryanweinegaessel@gmail.com>
2024-10-23 11:59:22 +0200

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -random number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
