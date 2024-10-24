a7ebafeda0abfa52d41e121401b870b555712798
mrfouu <bryanweinegaessel@gmail.com>
2024-10-24 14:27:40 +0200

    task 0 _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/**
+ * main -print _putchar
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	-putchar(r);
+
+	return(0);
+}
+
+	
