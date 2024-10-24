f27ad4fa09ca218fb112070f69743ba26a03cec4
mrfouu <bryanweinegaessel@gmail.com>
2024-10-24 14:52:13 +0200

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	-putchar(r);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	return(0);
+	return (0);
 }
 
 	
