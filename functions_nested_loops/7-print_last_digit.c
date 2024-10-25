cfa702b37a9498ad0910129151c9a8dfaeda8470
mrfouu <bryanweinegaessel@gmail.com>
2024-10-25 11:11:49 +0200

    task 7 test

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - function that print las digit of a number
+ *
+ * @n: umber to targeted
+ *
+ * Return: returns 0
+ */
+int print_last_digit(int n)
+{
+	int lastdigit = n % 10;
+
+	if (lastdigit < 0)
+
+		lastdigit *= -1;
+	_putchar (lastdigit + '0');
+
+	return (lasdigit);
+}
