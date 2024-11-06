c334f7c121e52967660f96a23299e0085edf7029
mrfouu <bryanweinegaessel@gmail.com>
2024-11-06 14:59:35 +0100

    task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,43 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
-void reset_to_98(int n);
-void swap_int(inta, int b);
-size_t _strlen(const charstr);
-void _puts(char str);
-void print_rev(chars);
-void rev_string(char s);
-void puts2(charstr);
-void puts_half(char str);
-void print_array(inta, int n);
-char _strcpy(chardest, char src);
-int _atoi(chars);
 
-int _putchar(char c);
-char _strcat(chardest, char src);
-char_strncat(char dest, charsrc, int n);
-char _strncpy(chardest, char src, int n);
-int _strcmp(chars1, char s2);
-void reverse_array(inta, int n);
-char string_toupper(char);
-char cap_string(char);
-char leet(char);
-char rot13(char);
-void print_number(int n);
-char infinite_add(charn1, char n2, charr, int size_r);
-void print_buffer(char b, int size);
+/* ---------- Pointers, arrays and strings ---------- */
 
-int _putchar(char c);
-char_memset(char s, char b, unsigned int n);
-char_memcpy(char dest, charsrc, unsigned int n);
-char _strchr(chars, char c);
-unsigned int _strspn(char s, characcept);
-char _strpbrk(chars, char accept);
-char_strstr(char haystack, charneedle);
-void print_chessboard(char (a)[8]);
-void set_string(char **s, charto);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
+/* ----- Even more pointers, arrays and strings ----- */
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 #endif
