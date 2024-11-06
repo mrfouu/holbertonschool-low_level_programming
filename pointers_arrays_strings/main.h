b2d61752edf64acafb1aa2559bd62b669d5771a8
mrfouu <bryanweinegaessel@gmail.com>
2024-11-06 14:36:27 +0100

    test task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,16 +3,41 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void reset_to_98(int n);
+void swap_int(inta, int b);
+size_t _strlen(const charstr);
+void _puts(char str);
+void print_rev(chars);
+void rev_string(char s);
+void puts2(charstr);
+void puts_half(char str);
+void print_array(inta, int n);
+char _strcpy(chardest, char src);
+int _atoi(chars);
+
+int _putchar(char c);
+char _strcat(chardest, char src);
+char_strncat(char dest, charsrc, int n);
+char _strncpy(chardest, char src, int n);
+int _strcmp(chars1, char s2);
+void reverse_array(inta, int n);
+char string_toupper(char);
+char cap_string(char);
+char leet(char);
+char rot13(char);
+void print_number(int n);
+char infinite_add(charn1, char n2, charr, int size_r);
+void print_buffer(char b, int size);
+
+int _putchar(char c);
+char_memset(char s, char b, unsigned int n);
+char_memcpy(char dest, charsrc, unsigned int n);
+char _strchr(chars, char c);
+unsigned int _strspn(char s, characcept);
+char _strpbrk(chars, char accept);
+char_strstr(char haystack, charneedle);
+void print_chessboard(char (a)[8]);
+void set_string(char **s, charto);
+void print_diagsums(int *a, int size);
 
 #endif
