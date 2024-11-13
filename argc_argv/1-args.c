76227fc2df1c3d978b57c5a981193bdd10301321
mrfouu <bryanweinegaessel@gmail.com>
2024-11-13 16:07:26 +0100

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: size of argv
+ * @argv: array of size argc
+ * Return: argv
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
