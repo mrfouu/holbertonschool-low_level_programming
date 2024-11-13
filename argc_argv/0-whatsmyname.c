26576da3cbfda80eba4a958398bf67eaaa2b583d
mrfouu <bryanweinegaessel@gmail.com>
2024-11-13 11:54:45 +0100

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: size of argv
+ * @argv: array of size argc
+ * Return: argv
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
