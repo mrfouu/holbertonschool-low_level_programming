388daf748405c71f5ed56f27aa1957c08c829f32
mrfouu <bryanweinegaessel@gmail.com>
2024-11-14 16:07:32 +0100

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,19 +10,6 @@
 
 char *_strdup(char *str)
 {
-/*
-	int size = 0;
-	int i = 0;
-	char *array = malloc((size + 1) * sizeof(char));
-
-	if (str == NULL || array == NULL)
-		return (NULL);
-	while (str[size] != '\0')
-		size++;
-	for (; i < size; i++)
-		array[i] = str[i];
-	return (array);
-*/
 	int i = 0;
 	char *array = malloc((i + 1) * sizeof(char));
 
