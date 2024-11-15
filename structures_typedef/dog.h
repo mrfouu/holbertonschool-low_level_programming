ba86a64eb62ab8d7c75cf96b5103c6061af7d55e
mrfouu <bryanweinegaessel@gmail.com>
2024-11-15 16:40:28 +0100

    task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - metadatas about a dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+#endif
