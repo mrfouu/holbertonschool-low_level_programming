dcbad1a861a38bf653fb61b6b431558e21469285
mrfouu <bryanweinegaessel@gmail.com>
2024-11-15 12:24:16 +0100

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*free_grid - Entry point
+*@grid: tableau
+*@height: longueur
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i <= height; i++)
+	{
+	    free(grid[(height) - i]);
+	}
+	free(grid);
+}
