8af5763c8bcedc756bce56552b73fcea23077769
mrfouu <bryanweinegaessel@gmail.com>
2024-11-15 13:10:43 +0100

    correction task 4 projet 1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,25 +13,27 @@ char *str_concat(char *s1, char *s2)
 {
 	int size1 = 0;
 	int size2 = 0;
-	char *result = malloc((size1 + size2 + 1) * sizeof(char));
 	int i = 0;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (result == NULL)
-		return (NULL);
 	while (s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
 		size2++;
+	result = malloc((size1 + size2 + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
 	for (; i <= (size1 + size2); i++)
 	{
 		if (i < size1)
 			result[i] = s1[i];
 		else
-			 result[i] = s2[i - (size1)];
+			result[i] = s2[i - (size1)];
 	}
+	result[i] = '\0';
 	return (result);
 }
