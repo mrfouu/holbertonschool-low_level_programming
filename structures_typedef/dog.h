ce0eb4bc1a7a7acd49336c41a8b8fcb715c60706
mrfouu <bryanweinegaessel@gmail.com>
2024-11-15 16:36:45 +0100

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,6 @@
-#ifndef DOG_FILE
-#define DOG_FILE
-/**
- * struct dog - the Struct type for scooby
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the name of dog's owner
- */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+}
