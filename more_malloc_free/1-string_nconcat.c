abe6f4989d29c86589fd33e261605ce628a41c34
mrfouu <bryanweinegaessel@gmail.com>
2024-11-15 14:11:51 +0100

    task1 correctif

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,31 +12,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *result;
 	unsigned int size1 = 0;
 	unsigned int size2 = 0;
-	unsigned int i = 0;
-	char *result;
+	unsigned int i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
 		size2++;
+
 	if (n < size2)
 		size2 = n;
 	result = malloc(size1 + size2 + 1);
 	if (result == 0)
-		return (NULL);
-	for (; i <= (size1 + size2); i++)
 	{
-		if (i < size1)
-			result[i] = s1[i];
-		else
-			result[i] = s2[i - (size1)];
+		return (NULL);
 	}
-	result[i] = '\0';
+
+	for (i = 0; i < size1; i++)
+	{
+		result[i] = s1[i];
+	}
+
+	for (i = 0; i < size2; i++)
+	{
+		result[i + size1] = s2[i];
+	}
+
+	result[size1 + i] = '\0';
 	return (result);
 }
