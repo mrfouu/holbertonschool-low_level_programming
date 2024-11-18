cc8efbc062bb71dccbb864312870bb1b91617b52
mrfouu <bryanweinegaessel@gmail.com>
2024-11-18 10:27:01 +0100

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - ecrire le nom
+ * @f: pointeur vers ma fonctions
+ * @name: nom
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
